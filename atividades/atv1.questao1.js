58fbca9272d758ca8febc2fb05109643d02b620d
daniel_caldeira <76625872+Danniel-bc@users.noreply.github.com>
2023-07-03 22:19:03 -0300

    Update atv1.questao1.js

---
+++ atividades/atv1.questao1.js
@@ -0,0 +1,25 @@
+function questao1(number){
+
+    var n1 = new Number (7);
+    var n2 = new Number (-55);
+    var n3 = new Number (389);
+    var n4 = new Number (1000);
+    var n5 = new Number (3.1459);
+    var n6 = new Number (-46.267);
+    console.log(n1, n2, n3, n4, n5, n6);
+
+    var n1 = new Number (7);
+    var n6= new Number (-46267);
+    let soma = n1 + n6;
+    console.log(n1, "+", n6, '=', soma);
+
+    var n1 = new Number(7);
+    var n3 = new Number(389);
+    let mult = n1 * n3
+    console.log(n1, "x", n3, "=", mult);
+
+    var n4 = new Number(1000);
+    var n5 = new Number(3.1459);
+    let divisao = n4 / n5;
+    console.log(n4, "/", n5, "=", divisao);
+}
